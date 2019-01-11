11cbbcd2ab61922de192bb75112d3cdab0f03992
Kris Kim <kriskim8937@gmail.com>
2019-01-12 05:03:51 +0900

    Updates

---
+++ precache-manifest.673b8f8708837593c2ca171a1d8ffdc0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "be7cabb7b2a20eaeb610",
+    "url": "/movie_app/static/css/main.2faba5d6.chunk.css"
+  },
+  {
+    "revision": "be7cabb7b2a20eaeb610",
+    "url": "/movie_app/static/js/main.be7cabb7.chunk.js"
+  },
+  {
+    "revision": "0e493f63e1ac10c82d37",
+    "url": "/movie_app/static/js/1.0e493f63.chunk.js"
+  },
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "866ad589253febca639f662baf77fcf6",
+    "url": "/movie_app/index.html"
+  }
+];
